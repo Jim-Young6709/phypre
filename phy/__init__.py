4b754ddb111d89407655b5c9c88949fb33c1035c
Jim-Young6709 <jiahuiyang6709@outlook.com>
2026-09-08 20:33:57 -0700

    register hydra path into __init__

--- phy/__init__.py
+++ phy/__init__.py
@@ -1,2 +1,23 @@
 """Physics pretraining environments and utilities."""
 
+import gymnasium as gym
+
+# env registration
+gym.register(
+    id="Phy-Franka-Table-Direct-v0",
+    entry_point="phy.franka_table:FrankaTableEnv",
+    disable_env_checker=True,
+    kwargs={"env_cfg_entry_point": "phy.cfg.franka_table_env_cfg:FrankaTableEnvCfg"},
+)
+
+# datagen registration
+gym.register(
+    id="Phy-Franka-Table-Datagen-v0",
+    entry_point="phy.franka_table:FrankaTableEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": (
+            "phy.cfg.franka_table_datagen_cfg:FrankaTableDatagenCfg"
+        ),
+    },
+)
